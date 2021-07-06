ad394fc157047bd48d72ad2182ce6b05e53a5620
MRXover <14204072+MRXover@users.noreply.github.com>
2021-07-06 15:39:19 +0300

    Add files via upload

---
+++ src/main/java/DAO/DAO.java
@@ -0,0 +1,10 @@
+package DAO;
+
+import model.Client;
+
+public interface DAO {
+
+    Client Authenticate(String login, String password) throws Exception;
+
+
+}
---
+++ src/main/java/model/DTODepositRequest.java
@@ -0,0 +1,17 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public class DTODepositRequest {
+    @JsonValue
+    private double deposit;
+
+
+    public double getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(double deposit) {
+        this.deposit = deposit;
+    }
+}
